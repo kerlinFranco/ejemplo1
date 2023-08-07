c32ee0c458262e16a1a9d65f09c52b8b11dcf9e5
kerlinFranco <kerlinfranco@gmail.com>
2023-08-07 20:09:46 -0300

    asada

---
+++ src/ejemplo1/algo/clase2.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejemplo1.algo;
+
+/**
+ *
+ * @author USURIO
+ */
+public class clase2 {
+    private char s;
+    
+}
---
+++ src/ejemplo1/clase1.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejemplo1;
+
+/**
+ *
+ * @author USURIO
+ */
+public class clase1 {
+    private int num;
+    private String letra;
+
+    public clase1(int num, String letra) {
+        this.num = num;
+        this.letra = letra;
+    }
+
+    public int getNum() {
+        return num;
+    }
+
+    public void setNum(int num) {
+        this.num = num;
+    }
+
+    public String getLetra() {
+        return letra;
+    }
+
+    public void setLetra(String letra) {
+        this.letra = letra;
+    }
+    
+    
+}
